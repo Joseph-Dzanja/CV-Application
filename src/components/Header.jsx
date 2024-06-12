e86e8f68ca0bf55016fd6536396bac3ab9cf5183
Joseph Dzanja <jhdzanja@gmail.com>
2024-06-12 15:22:11 +0200

    added basic components, made header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import '../styles/header.css'
+
+export default function Header(){
+    return(
+        <header className="header">
+            <h1>CV APPLICATION</h1>
+        </header>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Header from './components/Header'
+import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <Header />
   </React.StrictMode>,
 )
